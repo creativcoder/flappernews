e074e1f47993b0c29d8da641dd063195e1464be0
creativcoder <rsconceptx@gmail.com>
2015-07-02 21:36:17 +0530

    changed the models

--- app.js
+++ app.js
@@ -27,26 +27,7 @@ app.factory('posts',[function(){
 app.controller('MainCtrl',
     ['$scope','posts',function($scope,posts){
 
-        $scope.consumers = [
-        {   'id':'0',
-            'name':'Chotu',
-            'address':'Cage',
-            'phone':09176246016,
-            'email':'nehasharma.s@sybrant.com',
-            'credit':120,
-            'total_order':[24,'24th Jan 2015'],
-            'total_water':['34l','24th Jan 2015']
-        },
-        {   'id':'1',
-            'name':'Asha',
-            'address':'Home',
-            'phone':09176846070,
-            'email':'aashasweetu@gmail.com',
-            'credit':10,
-            'total_order':[150,'30th Jun 2015'],
-            'total_water':['348l','24th Jan 2015']
-        }
-        ];
+        
 
         $scope.posts = posts.posts;
 
